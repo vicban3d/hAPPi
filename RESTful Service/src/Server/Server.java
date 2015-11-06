3b09c28a1863e32442e9d60961e7cc33029a44f4
Victor <vicban3d@gmail.com>
2015-11-06 18:10:12 +0200

    Created a simple server

--- RESTful Service/src/Server/Server.java
+++ RESTful Service/src/Server/Server.java
@@ -10,19 +10,22 @@ import java.io.IOException;
  * Created by victor on 11/6/2015.
  */
 // The Java class will be hosted at the URI path "/main"
-@Path("/main")
+@Path("/hAPPi")
 public class Server {
     private static String SRV_HOST =  "http://localhost";
     private static String SRV_PORT =  "9998";
     private static String PATH_MAIN =  "/main";
 
-    // The Java method will process HTTP GET requests
     @GET
-    // The Java method will produce content identified by the MIME Media type "text/htm"
+    @Path("/main")
     @Produces("text/html")
     public String getMainPage() {
+        return getPage("index.html");
+    }
+
+    private String getPage(String src){
         try {
-            return Util.FileHandler.readFile("C:\\Users\\victor\\IdeaProjects\\HAPPY Server\\RESTful Service\\src\\Web\\index.html");
+            return Util.FileHandler.readFile("C:\\Users\\victor\\IdeaProjects\\HAPPY Server\\RESTful Service\\src\\Web\\" + src);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -34,7 +37,7 @@ public class Server {
         server.start();
 
         System.out.println("Server running");
-        System.out.println("Visit: " + SRV_HOST + ":" + SRV_PORT + PATH_MAIN);
+        System.out.println("Visit: " + SRV_HOST + ":" + SRV_PORT + "/hAPPi" + PATH_MAIN);
         System.out.println("Hit return to stop...");
         System.in.read();
         System.out.println("Stopping server");
