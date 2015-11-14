58dd3922852504184445c89a7770308f8ef2a3e6
Victor <vicban3d@gmail.com>
2015-11-14 10:32:57 +0200

    Fixed add android function

--- REST Server/src/Utility/Strings.java
+++ REST Server/src/Utility/Strings.java
@@ -13,12 +13,14 @@ public interface Strings {
     String SRV_PORT = "9998";
     String SRV_MAIN = "/hAPPi";
     String SRV_FULL = SRV_HOST + ":" + SRV_PORT + SRV_MAIN;
-    String PATH_WEB_CONTENT = "C:\\Users\\Gila-Ber\\IdeaProjects\\hAPPi\\REST Server\\src\\Web\\";
-    String PATH_CORDOVA = "C:\\Users\\Gila-Ber\\AppData\\Roaming\\npm\\cordova.cmd";
-    String PATH_PROJECTS = "C:\\Users\\Gila-Ber\\HAPPI\\Projects";
+    String PATH_WEB_CONTENT = "C:\\users\\victor\\IdeaProjects\\hAPPi\\REST Server\\src\\Web\\";
+    String PATH_CORDOVA = "C:\\users\\victor\\AppData\\Roaming\\npm\\cordova.cmd";
+    String PATH_PROJECTS = "C:\\users\\victor\\HAPPI\\Projects";
     String PATH_MAIN = "/main";
     String PATH_CREATE_PROJECT = "/createProject";
     String PATH_ADD_PLATFORM_ANDROID = "/addAndroid";
+    String PATH_ADD_PLATFORM_IOS = "/addIOS";
+    String PATH_ADD_PLATFORM_WINDOWS_PHONE = "/addWindowsPhone";
     String PATH_BUILD_PROJECT = "/build";
     String PATH_CREATE_ENTITY = "/createEntity";
     String DB_NAME = "hAPPiDB";
